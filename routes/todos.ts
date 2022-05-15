d98721da471d806fc93b0fc00de776dd8b181bcc
Maxpayne <ferhatadibelli9@gmail.com>
2022-05-15 18:35:40 +0300

    post route added

--- routes/todos.ts
+++ routes/todos.ts
@@ -8,4 +8,12 @@ route.get("/", (req, res, next) => {
   res.status(200).json({ todos: todos });
 });
 
+route.post("/post", (req, res, next) => {
+  const newTodo: Todo = {
+    id: new Date().toISOString(),
+    text: req.body.text,
+  };
+  todos.push(newTodo);
+});
+
 export default route;
