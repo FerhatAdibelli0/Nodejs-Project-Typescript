189248306ec8d163c8bd6b79428366a9e2a19a4b
Maxpayne <ferhatadibelli9@gmail.com>
2022-05-15 22:43:20 +0300

    added type request body and params

--- routes/todos.ts
+++ routes/todos.ts
@@ -4,24 +4,30 @@ import { Todo } from "../models/todo";
 const route = Router();
 let todos: Todo[] = [];
 
+type requestBody = { text: string };
+type requestParam = { postId: string };
+
 route.get("/", (req, res, next) => {
   res.status(200).json({ todos: todos });
 });
 
 route.post("/post", (req, res, next) => {
+  const body = req.body as requestBody;
   const newTodo: Todo = {
     id: new Date().toISOString(),
-    text: req.body.text,
+    text: body.text,
   };
   todos.push(newTodo);
   return res.status(201).json({ message: "Posted successfully", todos: todos });
 });
 
 route.put("/todo/:postId", (req, res, next) => {
-  const toID = req.params.postId;
+  const body = req.body as requestBody;
+  const params = req.params as requestParam;
+  const toID = params.postId;
   const findIndex = todos.findIndex((todo) => todo.id === toID);
   if (findIndex >= 0) {
-    todos[findIndex] = { id: todos[findIndex].id, text: req.body.text };
+    todos[findIndex] = { id: todos[findIndex].id, text: body.text };
     return res
       .status(201)
       .json({ message: "Successfully putted", todos: todos });
@@ -30,7 +36,8 @@ route.put("/todo/:postId", (req, res, next) => {
 });
 
 route.delete("/todo/:postId", (req, res, next) => {
-  const toID = req.params.postId;
+  const params = req.params as requestParam;
+  const toID = params.postId;
   todos = todos.filter((todo) => todo.id !== toID);
   res.status(201).json({ message: "Todo is deleted", todos: todos });
 });
